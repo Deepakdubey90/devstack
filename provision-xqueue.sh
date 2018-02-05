33053a38ae0d681f052624b1c54047d4978ebe57
Kevin Falcone <kevin@edx.org>
2018-02-05 11:12:09 -0500

    Have provisioning set up the rabbit users needed

--- provision-xqueue.sh
+++ provision-xqueue.sh
@@ -2,6 +2,8 @@ set -e
 set -o pipefail
 set -x
 
+# Bring up RabbitMQ and XQueue
+docker-compose $DOCKER_COMPOSE_FILES up -d rabbitmq
 docker-compose $DOCKER_COMPOSE_FILES up -d xqueue
 
 # This works in case you provision xqueue without having other services up
@@ -22,4 +24,8 @@ docker-compose $DOCKER_COMPOSE_FILES exec xqueue bash -c 'source /edx/app/xqueue
 # Add users that graders use to fetch data, there's one default user in Ansible which is part of our settings
 docker-compose $DOCKER_COMPOSE_FILES exec xqueue bash -c 'source /edx/app/xqueue/venvs/xqueue/bin/activate && cd /edx/app/xqueue/xqueue && SERVICE_VARIANT=xqueue python manage.py update_users --settings=xqueue.aws_settings'
 
-# Need to create some default grader users?
+# Create a user that can be used by xqueue to create / manage queues
+docker-compose $DOCKER_COMPOSE_FILES exec rabbitmq bash -c 'rabbitmqctl delete_user guest'
+docker-compose $DOCKER_COMPOSE_FILES exec rabbitmq bash -c 'rabbitmqctl add_user edx edx'
+docker-compose $DOCKER_COMPOSE_FILES exec rabbitmq bash -c 'rabbitmqctl set_permissions edx ".*" ".*" ".*"'
+docker-compose $DOCKER_COMPOSE_FILES exec rabbitmq bash -c 'rabbitmqctl set_user_tags edx administrator'
